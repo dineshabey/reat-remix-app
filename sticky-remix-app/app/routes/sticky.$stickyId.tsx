b8393ed4e526ab55d756e6f9217a7112529b0178
dineshabey <dinabeysinghe@gmail.com>
2023-04-20 19:46:25 +0530

    add remix validation

---
+++ sticky-remix-app/app/routes/sticky.$stickyId.tsx
@@ -0,0 +1,27 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { Link, useLoaderData } from "@remix-run/react";
+
+import { db } from "~/utils/db.server";
+
+export const loader = async ({ params }: LoaderArgs) => {
+  const joke = await db.sticky.findUnique({
+    where: { id: params.stickyId },
+  });
+  if (!joke) {
+    throw new Error("Joke not found");
+  }
+  return json({ joke });
+};
+
+export default function JokeRoute() {
+  const data = useLoaderData<typeof loader>();
+
+  return (
+    <div>
+      <p>Here's your hilarious joke:</p>
+      <Link to=".">"{data.joke.note}" Permalink</Link>
+      <p>{data.joke.content}</p>
+    </div>
+  );
+}
--- sticky-remix-app/app/routes/sticky.new.tsx
+++ sticky-remix-app/app/routes/sticky.new.tsx
@@ -1,26 +1,112 @@
-export default function NewStickyRoute() {
-    return (
-      <div>
-        <p>Add your own hilarious joke</p>
-        <form method="post">
-          <div>
-            <label>
-              Name: <input type="text" name="name" />
-            </label>
-          </div>
-          <div>
-            <label>
-              Content: <textarea name="content" />
-            </label>
-          </div>
-          <div>
-            <button type="submit" className="button">
-              Add
-            </button>
-          </div>
-        </form>
-      </div>
-    );
-  }
-  
+import type { ActionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useActionData } from "@remix-run/react";
 
+import { db } from "~/utils/db.server";
+import { badRequest } from "~/utils/request.server";
+
+function validateJokeContent(content: string) {
+  if (content.length < 10) {
+    return "That content is too short";
+  }
+}
+
+function validateJokeName(note: string) {
+  if (note.length < 3) {
+    return "That note is too short";
+  }
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const form = await request.formData();
+  const content = form.get("content");
+  const note = form.get("note");
+
+
+  if (typeof content !== "string" || typeof note !== "string") {
+    return badRequest({
+      fieldErrors: null,
+      fields: null,
+      formError: "Form not submitted correctly.",
+    });
+  }
+
+  const fieldErrors = {
+    content: validateJokeContent(content),
+    note: validateJokeName(note),
+  };
+  const fields = { content, note };
+  if (Object.values(fieldErrors).some(Boolean)) {
+    return badRequest({
+      fieldErrors,
+      fields,
+      formError: null,
+    });
+  }
+
+  const sticky = await db.sticky.create({ data: fields });
+  return redirect(`/sticky/${sticky.id}`);
+};
+
+export default function NewJokeRoute() {
+  const actionData = useActionData<typeof action>();
+
+  return (
+    <div>
+      <p>Add your own hilarious joke</p>
+      <form method="post">
+        <div>
+          <label>
+            Note:{" "}
+            <input
+              defaultValue={actionData?.fields?.note}
+              name="note"
+              type="text"
+              aria-invalid={Boolean(actionData?.fieldErrors?.note)}
+              aria-errormessage={
+                actionData?.fieldErrors?.note ? "note-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.fieldErrors?.note ? (
+            <p className="form-validation-error" id="note-error" role="alert">
+              {actionData.fieldErrors.note}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          <label>
+            Content:{" "}
+            <textarea
+              defaultValue={actionData?.fields?.content}
+              name="content"
+              aria-invalid={Boolean(actionData?.fieldErrors?.content)}
+              aria-errormessage={
+                actionData?.fieldErrors?.content ? "content-error" : undefined
+              }
+            />
+          </label>
+          {actionData?.fieldErrors?.content ? (
+            <p
+              className="form-validation-error"
+              id="content-error"
+              role="alert"
+            >
+              {actionData.fieldErrors.content}
+            </p>
+          ) : null}
+        </div>
+        <div>
+          {actionData?.formError ? (
+            <p className="form-validation-error" role="alert">
+              {actionData.formError}
+            </p>
+          ) : null}
+          <button type="submit" className="button">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+}
